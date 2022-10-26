3d1a04a0585d005fdbb4733de53046a06dbb9ae3
federico.moreno1 <federico.moreno@kavak.com>
2022-10-26 20:49:00 -0300

    Arreglos

--- src/main.cpp
+++ src/main.cpp
@@ -9,104 +9,28 @@ void setup() {
         pinMode(i, OUTPUT);
     }
 };
-// queremos que encienda primero un numero y despues otro del 0 al 9
-// por cada numero tenemos que prender un segmento que lo represente
 
+// crear un arreglo para reemplazar el codigo repetitivo
+// seria asi el arreglo: tipo de variable + nombre de variable
+// arreglo de los numeros simples
+int numeros[10][7] = {
+    {0,0,0,0,0,0,1}, //0
+    {1,0,0,1,1,1,1}, //1
+    {0,0,1,0,0,1,0}, //2
+    {0,0,0,0,1,1,0}, //3
+    {1,0,0,1,1,0,0}, //4
+    {0,1,0,0,1,0,0}, //5
+    {0,1,0,0,0,0,0}, //6
+    {0,0,0,1,1,1,1}, //7
+    {0,0,0,0,0,0,0}, //8
+    {0,0,0,0,1,0,0}  //9
+};
+//prendo el led en funcion del arreglo
 void loop() {
-    //encender el numero 0, despues el 2 hasta el 9
-    //encender el 0 necesito todos los segmentos menos el del medio
-    digitalWrite(2, 0);
-    digitalWrite(3, 0);
-    digitalWrite(4, 0);
-    digitalWrite(5, 0);
-    digitalWrite(6, 0);
-    digitalWrite(7, 0);
-    digitalWrite(8, 1);
-    delay(500);
-    //encender el 1
-    digitalWrite(2, 1);
-    digitalWrite(3, 0);
-    digitalWrite(4, 0);
-    digitalWrite(5, 1);
-    digitalWrite(6, 1);
-    digitalWrite(7, 1);
-    digitalWrite(8, 1);
-    delay(500);
-    //encender el 2
-    digitalWrite(2, 0);
-    digitalWrite(3, 0);
-    digitalWrite(4, 1);
-    digitalWrite(5, 0);
-    digitalWrite(6, 0);
-    digitalWrite(7, 1);
-    digitalWrite(8, 0);
-    delay(500);
-    //encender el 3
-    digitalWrite(2, 0);
-    digitalWrite(3, 0);
-    digitalWrite(4, 0);
-    digitalWrite(5, 0);
-    digitalWrite(6, 1);
-    digitalWrite(7, 1);
-    digitalWrite(8, 0);
-    delay(500);
-    //encender el 4
-    digitalWrite(2, 1);
-    digitalWrite(3, 0);
-    digitalWrite(4, 0);
-    digitalWrite(5, 1);
-    digitalWrite(6, 1);
-    digitalWrite(7, 0);
-    digitalWrite(8, 0);
-    delay(500);
-    //encender el 5
-    digitalWrite(2, 0);
-    digitalWrite(3, 1);
-    digitalWrite(4, 0);
-    digitalWrite(5, 0);
-    digitalWrite(6, 1);
-    digitalWrite(7, 0);
-    digitalWrite(8, 0);
-    delay(500);
-    //encender el 6
-    digitalWrite(2, 0);
-    digitalWrite(3, 1);
-    digitalWrite(4, 0);
-    digitalWrite(5, 0);
-    digitalWrite(6, 0);
-    digitalWrite(7, 0);
-    digitalWrite(8, 0);
-    delay(500);
-    //encender el 7
-    digitalWrite(2, 0);
-    digitalWrite(3, 0);
-    digitalWrite(4, 0);
-    digitalWrite(5, 1);
-    digitalWrite(6, 1);
-    digitalWrite(7, 1);
-    digitalWrite(8, 1);
-    delay(500);
-    //encender el 8
-    digitalWrite(2, 0);
-    digitalWrite(3, 0);
-    digitalWrite(4, 0);
-    digitalWrite(5, 0);
-    digitalWrite(6, 0);
-    digitalWrite(7, 0);
-    digitalWrite(8, 0);
-    delay(500);
-    //encender el 9
-    digitalWrite(2, 0);
-    digitalWrite(3, 0);
-    digitalWrite(4, 0);
-    digitalWrite(5, 0);
-    digitalWrite(6, 1);
-    digitalWrite(7, 0);
-    digitalWrite(8, 0);
-    delay(500);
-
-
-
-
-
+    for (int i=0; i<10; i++) {
+        for (int j=2; j<=8; j++) {
+            digitalWrite(j, numeros[i][j-2]);
+        }
+        delay(500);
+    }
 }
