a12f8d9136321218d4cbc7464a669fdeab0580c1
federico.moreno1 <federico.moreno@kavak.com>
2022-10-26 21:47:17 -0300

    Finalizacion de Monitor serial

---
+++ code_examples/11_loop_numbers.cpp
@@ -0,0 +1,32 @@
+#include <Arduino.h>
+//create a code to turn on the led with number 0 to 9
+void setup() {
+    for (int i=2; i<=8; i++) {
+        pinMode(i, OUTPUT);
+    }
+};
+
+// crear un arreglo para reemplazar el codigo repetitivo
+// seria asi el arreglo: tipo de variable + nombre de variable
+// arreglo de los numeros simples
+bool numeros[10][7] = {
+    {0,0,0,0,0,0,1}, //0
+    {1,0,0,1,1,1,1}, //1
+    {0,0,1,0,0,1,0}, //2
+    {0,0,0,0,1,1,0}, //3
+    {1,0,0,1,1,0,0}, //4
+    {0,1,0,0,1,0,0}, //5
+    {0,1,0,0,0,0,0}, //6
+    {0,0,0,1,1,1,1}, //7
+    {0,0,0,0,0,0,0}, //8
+    {0,0,0,0,1,0,0}  //9
+};
+//prendo el led en funcion del arreglo
+void loop() {
+    for (int i=0; i<10; i++) {
+        for (int j=0; j<7; j++) {
+            digitalWrite(j+2, numeros[i][j]);
+        }
+        delay(500);
+    }
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,36 +1,43 @@
 #include <Arduino.h>
-
-
-#include <Arduino.h>
-
 //create a code to turn on the led with number 0 to 9
 void setup() {
     for (int i=2; i<=8; i++) {
         pinMode(i, OUTPUT);
+        //serial begin
+        Serial.begin(9600);
     }
 };
 
 // crear un arreglo para reemplazar el codigo repetitivo
 // seria asi el arreglo: tipo de variable + nombre de variable
 // arreglo de los numeros simples
-int numeros[10][7] = {
-    {0,0,0,0,0,0,1}, //0
-    {1,0,0,1,1,1,1}, //1
-    {0,0,1,0,0,1,0}, //2
-    {0,0,0,0,1,1,0}, //3
-    {1,0,0,1,1,0,0}, //4
-    {0,1,0,0,1,0,0}, //5
-    {0,1,0,0,0,0,0}, //6
-    {0,0,0,1,1,1,1}, //7
-    {0,0,0,0,0,0,0}, //8
-    {0,0,0,0,1,0,0}  //9
+bool numeros[10][7] = {
+        {0,0,0,0,0,0,1}, //0
+        {1,0,0,1,1,1,1}, //1
+        {0,0,1,0,0,1,0}, //2
+        {0,0,0,0,1,1,0}, //3
+        {1,0,0,1,1,0,0}, //4
+        {0,1,0,0,1,0,0}, //5
+        {0,1,0,0,0,0,0}, //6
+        {0,0,0,1,1,1,1}, //7
+        {0,0,0,0,0,0,0}, //8
+        {0,0,0,0,1,0,0}  //9
 };
-//prendo el led en funcion del arreglo
+
+//vamos a trasmitir un numero por el puerto serial sin un loop
+int incomingByte = 1; // for incoming serial data
+
 void loop() {
-    for (int i=0; i<10; i++) {
-        for (int j=2; j<=8; j++) {
-            digitalWrite(j, numeros[i][j-2]);
+    // send data only when you receive data:
+    if (Serial.available() > 0) {
+        // read the incoming byte:
+        int num = Serial.read();
+        num -= 48;
+        if (num >= 0 && num <= 9) {
+            for (int j=0; j<7; j++) {
+                digitalWrite(j+2, numeros[num][j]);
+            }
         }
-        delay(500);
+
+        }
     }
-}
